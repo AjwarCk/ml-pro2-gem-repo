b3661eab5bdc067b9d2ed4c2a5776e26b4488b0d
AjwarCk <ajwar.trn@outlook.com>
2025-03-01 19:07:24 +0530

    setup update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name = 'mlproject_regression',
+    name = 'ml-project-gem_price-prediction-regression',
     version = '0.0.1',
     author = 'Ajwar CK',
     packages = find_packages(),
