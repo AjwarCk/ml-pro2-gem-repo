85ed75b0ecce93e2eb5215b6348ee6c45d0bd2b8
AjwarCk <ajwar.trn@outlook.com>
2025-03-02 17:39:12 +0530

    project structure update1

--- setup.py
+++ setup.py
@@ -5,28 +5,28 @@ HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return list of requirement
+    This function will return list of requirements
     from requirements.txt file
     '''
 
     # Initializing blank requirements list
     requirements = []
 
-    # Opening the file
+    # Opening the file.
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
 
-        # Remove hyphen_e_dot if present in requirements
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        # Remove e_dot if present in requirements.txt
+        requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
 setup(
-    name = 'ml-project-gem_price-prediction-regression',
+    name = 'ml-project-gem_price_prediction-regression',
     version = '0.0.1',
     author = 'Ajwar CK',
+    author_email= 'ajwar.trn@outlook.com',
     packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')   
+    install_requires = get_requirements('requirements.txt')
 )
